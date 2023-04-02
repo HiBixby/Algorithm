548231865acef7a2696bfee165f54c27d8566a7b
HiBixby <201910789@sangmyung.kr>
2023-04-02 11:11:48 +0900

    🎨 Apply a prettier

--- programmers/javascript/JadenCase 문자열 만들기.js
+++ programmers/javascript/JadenCase 문자열 만들기.js
@@ -1,11 +1,14 @@
 function solution(s) {
-    answer = s.split(" ").map(word=>{
-        if(word.length==0){
-            return word;
-        }else{
-            word = word.toLowerCase();
-            return word[0].toUpperCase()+word.slice(1);
-        }
-    }).join(" ");
-    return answer;
+  answer = s
+    .split(" ")
+    .map((word) => {
+      if (word.length == 0) {
+        return word;
+      } else {
+        word = word.toLowerCase();
+        return word[0].toUpperCase() + word.slice(1);
+      }
+    })
+    .join(" ");
+  return answer;
 }
