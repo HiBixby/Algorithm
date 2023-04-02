2d6d9ee9ae5e51d0ffa16db7656ac6a8120d3506
HiBixby <201910789@sangmyung.kr>
2023-04-02 11:11:08 +0900

    ♻️ Remove a map method in 최솟값 만들기.js

--- programmers/javascript/최솟값 만들기.js
+++ programmers/javascript/최솟값 만들기.js
@@ -1,7 +1,5 @@
 function solution(A, B) {
   A.sort((a, b) => a - b);
   B.sort((a, b) => b - a);
-  let AB = A.map((e, i) => e * B[i]);
-  let answer = AB.reduce((a, c) => a + c);
-  return answer;
+  return A.reduce((prev, curr, i) => prev + curr * B[i], 0);
 }
