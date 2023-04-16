f86073d840617c1d93ce37934ef1ecae6d47e8f1
HiBixby <201910789@sangmyung.kr>
2023-04-17 04:06:37 +0900

    ✨ 콜라스 추측.js

---
+++ programmers/javascript/Lv. 1/콜라스 추측.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let result = 0;
+  while (n !== 1) {
+    if (result > 500) return -1;
+    if (n % 2 == 0) {
+      n /= 2;
+    } else {
+      n = n * 3 + 1;
+    }
+    result++;
+  }
+  return result;
+}
