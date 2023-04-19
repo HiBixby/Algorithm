30e84baa4aa77fe264c17e2b251942cd2244fce5
HiBixby <201910789@sangmyung.kr>
2023-04-19 10:13:17 +0900

    ♻️ 변수명 변경

--- programmers/javascript/Lv. 2/[1차] 캐시.js
+++ programmers/javascript/Lv. 2/[1차] 캐시.js
@@ -2,18 +2,18 @@ function solution(cacheSize, cities) {
   let cache = Array.from({ length: cacheSize });
   let total = 0;
   for (let city of cities) {
-    let x = city.toLowerCase();
-    let idx = cache.indexOf(x);
+    city = city.toLowerCase();
+    let idx = cache.indexOf(city);
     if (idx !== -1) {
       // cache hit
       for (j = idx - 1; j > -1; j--) {
         cache[j + 1] = cache[j];
       }
-      cache[0] = x;
+      cache[0] = city;
       total++;
     } else {
       // cache miss
-      cache.unshift(x);
+      cache.unshift(city);
       cache.pop();
       total += 5;
     }
