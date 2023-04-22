97c6b0d1311f21b3d906afdce6a4e11ca506d9ff
HiBixby <201910789@sangmyung.kr>
2023-04-22 19:21:01 +0900

    🐛 n이 1일때 오답, 나머지 반환 관련 문제 해결

--- programmers/javascript/Lv. 2/멀리뛰기.js
+++ programmers/javascript/Lv. 2/멀리뛰기.js
@@ -1,7 +1,8 @@
 function solution(n) {
   let dp = [1, 2];
+  let DIV = 1234567;
   for (let i = 2; i < n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
+    dp[i] = (dp[i - 1] % DIV) + (dp[i - 2] % DIV);
   }
-  return dp.pop();
+  return dp[n - 1] % DIV;
 }
