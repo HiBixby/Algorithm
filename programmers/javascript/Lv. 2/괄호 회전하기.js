32a80b25588a1c0e096cbd64de2de98e3d0091b3
HiBixby <201910789@sangmyung.kr>
2023-04-23 14:03:50 +0900

    ✨ 괄호 회전하기.js

---
+++ programmers/javascript/Lv. 2/괄호 회전하기.js
@@ -0,0 +1,30 @@
+const rotate = (s) => {
+  s.push(s.shift());
+  return s;
+};
+const isCorrect = (s) => {
+  let bracket = { "}": "{", ")": "(", "]": "[" };
+  let stack = [];
+  s.forEach((e) => {
+    console.log(stack);
+    let l = stack[stack.length - 1];
+    if (bracket[e] !== undefined && stack[stack.length - 1] === bracket[e]) {
+      if (stack.length === 0) return false;
+      stack.pop();
+    } else {
+      stack.push(e);
+    }
+  });
+  return stack.length === 0;
+};
+function solution(s) {
+  s = [...s];
+  let answer = 0;
+  for (let i = 0; i < s.length; i++) {
+    answer += isCorrect(s) ? 1 : 0;
+    s = rotate(s);
+  }
+  return answer;
+}
+
+console.log(solution("}}}"));
