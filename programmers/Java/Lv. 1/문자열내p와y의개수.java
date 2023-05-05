a20823da80ee3a7556297a05e3730fde7968776b
HiBixby <201910789@sangmyung.kr>
2023-05-05 11:22:51 +0900

    ✨ 문자열내p와y의개수.java

---
+++ programmers/Java/Lv. 1/문자열내p와y의개수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+
+  boolean solution(String s) {
+    s = s.toLowerCase();
+    int pCnt = 0;
+    int yCnt = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == 'p') {
+        pCnt++;
+      } else if (s.charAt(i) == 'y') {
+        yCnt++;
+      }
+    }
+    return yCnt == pCnt;
+  }
+}
