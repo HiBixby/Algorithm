5ace53d6854e1f3014f43bf0f0a66e1610a52f0f
HiBixby <201910789@sangmyung.kr>
2023-05-13 09:02:26 +0900

    ✨ Counting Sort 1.py

---
+++ HackerRank/Counting Sort 1.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def countingSort(arr):
+    # Write your code here
+    n = len(arr)
+    d = [0]*100
+    for e in arr:
+        if e < 100:
+            d[e] += 1
+    return d
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
