830d6ba04245d7dc26258d373cae5b9a36680a32
HiBixby <201910789@sangmyung.kr>
2023-07-16 21:09:21 +0900

    🎨 스도쿠 검증.py

--- SW Expert Academy/D2/스도쿠 검증.py
+++ SW Expert Academy/D2/스도쿠 검증.py
@@ -1,30 +1,31 @@
 T = int(input())
-for tc in range(1,T+1):
-    dx = [-1,-1,-1,0,1,1,1,0]
-    dy = [-1,0,1,1,1,0,-1,-1]
-    stack = [(1,1),(1,4),(1,7),(4,1),(4,4),(4,7),(7,1),(7,4),(7,7)]
+for tc in range(1, T+1):
+    dx = [-1, -1, -1, 0, 1, 1, 1, 0]
+    dy = [-1, 0, 1, 1, 1, 0, -1, -1]
+    stack = [(1, 1), (1, 4), (1, 7), (4, 1), (4, 4),
+             (4, 7), (7, 1), (7, 4), (7, 7)]
     # 입력 받기
     stoku = []
     answer = 1
     for _ in range(9):
-        stoku.append(list(map(int,input().split())))
+        stoku.append(list(map(int, input().split())))
     for i in range(9):
         row = stoku[i]
-        col =  []
+        col = []
         for j in range(9):
             col.append(stoku[j][i])
-        if len(set(row))<9 or len(set(col))<9:
+        if len(set(row)) < 9 or len(set(col)) < 9:
             answer = 0
             break
     for i in range(9):
         square = set()
-        x,y = stack.pop()
+        x, y = stack.pop()
         square.add(stoku[x][y])
         for j in range(8):
             nx = x+dx[j]
             ny = y+dy[j]
             square.add(stoku[nx][ny])
-        if len(square)<9:
+        if len(square) < 9:
             answer = 0
             break
     print("#"+str(tc)+" "+str(answer))
